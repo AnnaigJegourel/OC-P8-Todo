75454a76565253e3ac4b1807fc9c1e6b5618f5e8
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-12 10:37:17 +0200

    Add isValid() to all form checks in TaskController & UserController

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -28,7 +28,7 @@ class TaskController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
 
             $em->persist($task);
@@ -51,7 +51,7 @@ class TaskController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
             $this->addFlash('success', 'La tâche a bien été modifiée.');
