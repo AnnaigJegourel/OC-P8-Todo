ac71d1fbdba4473a21a7768167c3ef2c12559ecf
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-12 10:10:06 +0200

    Fix "extends Controller" to "extends AbstractController"

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
