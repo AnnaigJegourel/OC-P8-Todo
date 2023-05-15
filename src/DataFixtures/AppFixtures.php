6ff99d47baf8bb60f295c76a4735255c6a611f05
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-15 17:25:01 +0200

    Create Fixtures in AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,65 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Task;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * Hash password
+     *
+     * @var UserPasswordHasherInterface
+     */
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    /**
+     * Fixtures constructor
+     *
+     * @param UserPasswordHasherInterface $userPasswordHasher
+     */
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    /**
+     * Load the fixtures
+     *
+     * @param ObjectManager $manager
+     *
+     * @return void
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Load users.
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User;
+
+            // $user->setRoles(['ROLE_USER']);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, '$i'*6));
+            $user->setEmail('number'.$i.'@user.com');
+            $user->setUsername('User'.$i);
+
+            $manager->persist($user);
+        }
+
+        // Load tasks.
+        for ($i = 0; $i < 20; $i++) {
+            $task = new Task;
+
+            $task->setTitle('Task'.$i);
+            $task->setContent('This is the description of task '.$i);
+            $task->setCreatedAt(new \DateTime());
+            for ($i = 0; $i < 8; $i++) {
+                $task->isDone();
+            }
+
+            $manager->persist($task);
+        }
 
         $manager->flush();
     }
