f72b150c22207a4cdf1c69b0a2d74f9ec88802c7
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-18 12:08:14 +0200

    Configure TaskVoter

--- src/Security/Voter/TaskVoter.php
+++ src/Security/Voter/TaskVoter.php
@@ -2,20 +2,27 @@
 
 namespace App\Security\Voter;
 
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class TaskVoter extends Voter
 {
-    public const EDIT = 'POST_EDIT';
-    public const VIEW = 'POST_VIEW';
+    public const MODIFY = 'TASK_MODIFY';
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
 
     protected function supports(string $attribute, mixed $subject): bool
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::EDIT, self::VIEW])
+        return in_array($attribute, [self::MODIFY])
             && $subject instanceof \App\Entity\Task;
     }
 
@@ -29,14 +36,20 @@ class TaskVoter extends Voter
 
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
-            case self::EDIT:
-                // logic to determine if the user can EDIT
-                // return true or false
+            case self::MODIFY:
+                // L'auteur peut modifier uniquement ses propres tâches.
+                if ($user === $subject->getAuthor()) {
+                    return true;
+                }
+                // Une tâche sans auteur (affiché 'anonyme') peut être modifiée uniquement par un/e admin.
+                if($subject->getAuthor() === null && $this->security->isGranted('ROLE_ADMIN')) {
+                    return true;
+                }
                 break;
-            case self::VIEW:
+            // case self::DELETE:
                 // logic to determine if the user can VIEW
                 // return true or false
-                break;
+                // break;
         }
 
         return false;
