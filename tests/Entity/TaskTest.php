3b2fb4ffcddb3a28bc91758c4c0c46ab137af781
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-25 16:54:55 +0200

    Create /tests/Entity/TaskTest.php

---
+++ tests/Entity/TaskTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    public function testsetCreatedAt()
+    {
+        // Créer l'objet.
+        // Appeler la méthode à tester.
+        // Faire une assertion pour le résultat attendu.
+    }
+
+    public function testisDone()
+    {
+
+    }
+
+    public function testgetAuthor()
+    {
+
+    }
+
+    public function testsetAuthor()
+    {
+
+    }
+
+}
