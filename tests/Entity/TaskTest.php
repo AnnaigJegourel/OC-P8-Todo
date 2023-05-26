f31b1d12fcc7847309a9c83c6f2bf6e8b2462877
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-26 14:08:29 +0200

    Write tests for Entity Task (U.T.)

--- tests/Entity/TaskTest.php
+++ tests/Entity/TaskTest.php
@@ -2,30 +2,35 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Task;
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class TaskTest extends TestCase
 {
-    public function testsetCreatedAt()
-    {
         // Créer l'objet.
         // Appeler la méthode à tester.
         // Faire une assertion pour le résultat attendu.
+
+    public function testTitle()
+    {
+        $task = new Task;
+        $task->setTitle('Testitle');
+        $this->assertSame($task->getTitle(), 'Testitle');
     }
 
     public function testisDone()
     {
-
+        $task = new Task; // constructeur: $isDone = false
+        $this->assertSame(false, $task->isDone());
     }
 
-    public function testgetAuthor()
+    public function testAuthorType()
     {
+        $task = new Task;
+        $author = new User;
 
+        $task->setAuthor($author);
+        $this->assertSame(true, $task->getAuthor() instanceof User);
     }
-
-    public function testsetAuthor()
-    {
-
-    }
-
 }
