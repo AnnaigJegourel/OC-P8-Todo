a924935b9a319155c74d927c20a853ee63e610d2
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-29 09:53:19 +0200

    Write DefaultControllerTest (testing the homepage)

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testHomepage(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur Todo List');
+        $this->assertResponseStatusCodeSame(200);
     }
 }
