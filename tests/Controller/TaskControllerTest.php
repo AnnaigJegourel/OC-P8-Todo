e2f9a5a686c19fef1dad6662f7b7232490779830
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-30 16:19:32 +0200

    Create TaskControllerTest factorized with setUp() & simulating logging

---
+++ tests/Controller/TaskControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TaskControllerTest extends WebTestCase
+{
+    private KernelBrowser|null $client = null;
+    private $testUser;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        // Use the user repository.
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        // Fetch the test user.
+        $this->testUser = $userRepository->findOneByUsername('User-0');
+    }
+
+    public function testTodoListPageIsUpWhileLoggedIn(): void
+    {        
+        // Simulate $testUser being logged in.
+        $this->client->loginUser($this->testUser);
+
+        $urlGenerator = $this->client->getContainer()->get('router.default');
+        $crawler = $this->client->request('GET', $urlGenerator->generate('task_list'));
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testDoneListPageIsUpWhileLoggedIn(): void
+    {
+        $this->client->loginUser($this->testUser);
+
+        $urlGenerator = $this->client->getContainer()->get('router.default');
+        $crawler = $this->client->request('GET', $urlGenerator->generate('task_done'));
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    // routes à tester 200: task_done, task_create, task_edit, task_toggle, task_delete
+    // tester les formulaires
+}
