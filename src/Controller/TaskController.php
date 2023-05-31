b3487e85da296af90fef90dabf6b6ddfd2013af1
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-05-31 16:31:36 +0200

    Add blank lines & remove useless comments in TaskController

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class TaskController extends AbstractController
 {
-    // condition: connecté / role user
     // #[IsGranted('IS_AUTHENTICATED')]
     #[Route(path: "/tasks", name: "task_list")]
     public function listAction(TaskRepository $taskRepository)
@@ -22,33 +21,32 @@ class TaskController extends AbstractController
                 'isDone' => 0
             ])
         ]);
+
     }
 
-    // condition: connecté / role user
+
     #[Route("/tasks/done", name: "task_done")]
     public function listDoneAction(TaskRepository $taskRepository)
     {
         return $this->render('task/list.html.twig', [
             'tasks' => $taskRepository->findBy([
-                // 'author' => $this->getUser(),
                 'isDone' => 1
             ])
         ]);
+
     }
 
-    // condition: connecté / role user
+
     #[Route(path: "/tasks/create", name: "task_create")]
     public function createAction(Request $request, TaskRepository $taskRepository)
     {
+
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $task->setAuthor($this->getUser());
-            
             $taskRepository->save($task, true);
             $this->addFlash('success', 'La tâche a été bien été ajoutée.');
 
@@ -56,25 +54,20 @@ class TaskController extends AbstractController
         }
 
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+
     }
+
 
     #[Route(path: "/tasks/{id}/edit", name: "task_edit")]
     #[IsGranted('TASK_MODIFY', subject: 'task')]
     public function editAction(Task $task, Request $request, TaskRepository $taskRepository)
     {
+
         $form = $this->createForm(TaskType::class, $task);
-
         $form->handleRequest($request);
-
-        // if ($task->getAuthor() !== $this->getUser()) {
-        //     $this->addFlash('error', 'Vous ne pouvez modifier que vos propres tâches.');
-
-        //     return $this->redirectToRoute('homepage');
-        // }
 
         if ($form->isSubmitted() && $form->isValid()) {
             $taskRepository->save($task, true);
-
             $this->addFlash('success', 'La tâche a bien été modifiée.');
 
             return $this->redirectToRoute('task_list');
@@ -84,40 +77,33 @@ class TaskController extends AbstractController
             'form' => $form->createView(),
             'task' => $task,
         ]);
+
     }
+
 
     #[Route(path: "/tasks/{id}/toggle", name: "task_toggle")]
     #[IsGranted('TASK_MODIFY', subject: 'task')]
     public function toggleTaskAction(Task $task, TaskRepository $taskRepository)
     {
-        // if ($task->getAuthor() !== $this->getUser()) {
-        //     $this->addFlash('error', 'Vous ne pouvez modifier que vos propres tâches.');
-
-        //     return $this->redirectToRoute('homepage');
-        // }
-
         $task->toggle(!$task->isDone());
         $taskRepository->save($task, true);
-
         $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
 
         return $this->redirectToRoute('task_list');
+
     }
+
 
     #[Route(path: "/tasks/{id}/delete", name: "task_delete")]
     #[IsGranted('TASK_MODIFY', subject: 'task')]
     public function deleteTaskAction(Task $task, TaskRepository $taskRepository)
     {
-        // if ($task->getAuthor() !== $this->getUser()) {
-        //     $this->addFlash('error', 'Vous ne pouvez supprimer que vos propres tâches.');
-
-        //     return $this->redirectToRoute('homepage');
-        // }
-
         $taskRepository->remove($task, true);
-
         $this->addFlash('success', 'La tâche a bien été supprimée.');
 
         return $this->redirectToRoute('task_list');
+
     }
+
+
 }
