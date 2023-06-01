b994dbc74b72ffcdfb1cfbda785883084195b7d8
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-06-01 15:06:27 +0200

    Improve SecurityController code style for Codacy

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,33 +9,52 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * Manage login form
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
     #[Route(path: "/login", name: "login")]
     public function loginAction(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
+        if ($this->getUser() === true) {
             return $this->redirectToRoute('homepage');
         }
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('security/login.html.twig', array(
+        return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
-        ));
+        ]);
 
     }
 
 
+    /**
+     * Check login
+     *
+     * @return void
+     */
     #[Route(path: "/login_check", name: "login_check")]
     public function loginCheck()
     {
         // This code is never executed.
+
     }
 
+
+    /**
+     * Check logout
+     *
+     * @return void
+     */
     #[Route(path: "/logout", name: "logout")]
     public function logoutCheck()
     {
         // This code is never executed.
+
     }
 
 
